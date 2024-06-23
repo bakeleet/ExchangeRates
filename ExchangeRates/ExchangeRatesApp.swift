e8f77ac41c7eaa0abd1cfc3bb786562b3763ba33
bakeleet <krzys.niestroj@gmail.com>
2024-06-23 22:16:29 +0200

    Initial Commit

---
+++ ExchangeRates/ExchangeRatesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExchangeRatesApp.swift
+//  ExchangeRates
+//
+//  Created by bakeleet on 23/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct ExchangeRatesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
